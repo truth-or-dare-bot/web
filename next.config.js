09f8ecb16b77dfebfaf7a5983fd90d788bdcc185
Coding <28990589+tandpfun@users.noreply.github.com>
2022-10-03 19:04:25 -0700

    feat: add r-bot invite

--- next.config.js
+++ next.config.js
@@ -14,10 +14,14 @@ const nextConfig = {
 			},
 			{
 				source: "/invite",
-				destination:
-					"https://discord.com/oauth2/authorize?client_id=692045914436796436&permissions=19456&scope=bot%20applications.commands",
+				destination: "https://discord.com/oauth2/authorize?client_id=692045914436796436&permissions=19456&scope=bot%20applications.commands",
 				permanent: true,
 			},
+			{
+				source: "/rinvite",
+				destination: "https://discord.com/oauth2/authorize?client_id=1017989345292058656&permissions=19456&scope=applications.commands%20bot",
+				permanent: true,
+			}
 		];
 	},
 	async rewrites() {
